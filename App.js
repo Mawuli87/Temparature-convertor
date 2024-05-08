a1b772d7810f71d15f3976cd7f708592e5703c78
Mawuli87 <yawomessie@gmail.com>
2024-05-08 10:07:09 +0000

    Temperature convertor

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Text, View, ImageBackground } from "react-native";
+import hotBackground from "./assets/hot.png";
+import coldBackground from "./assets/cold.png";
 
+import {
+  UNITS,
+  convertTemperatureTo,
+  getOppositeUnit,
+  isIceTemperature,
+} from "./utils/temperature";
+
+import { Input } from "./components/Input";
+import { s } from "./App.style";
+import { useEffect, useState } from "react";
+import { DisplayTemperature } from "./components/displayTemperature/DisplayTemperature";
+import { ButtonConvert } from "./components/buttonConvert/ButtonConvert";
 export default function App() {
+  const [inputValue, setInputValue] = useState(0);
+  const [currentUnit, setCurrentUnit] = useState("°C");
+  const oppositeUnit = getOppositeUnit(currentUnit);
+  const [currentBackground, setCurrentBackground] = useState(coldBackground);
+
+  useEffect(() => {
+    const isCold = isIceTemperature(inputValue, currentUnit);
+    setCurrentBackground(isCold ? coldBackground : hotBackground);
+  }, [inputValue, currentUnit]);
+
+  function getConvertedTemperature() {
+    if (isNaN(inputValue)) {
+      return "";
+    } else {
+      return convertTemperatureTo(inputValue, oppositeUnit).toFixed(1);
+    }
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <ImageBackground style={s.backgroundImg} source={currentBackground}>
+        <SafeAreaView style={s.root}>
+          <View style={s.workspace}>
+            <DisplayTemperature
+              unit={oppositeUnit}
+              temperature={getConvertedTemperature()}
+            />
+            <Input
+              unit={currentUnit}
+              onChange={setInputValue}
+              defaultValue={0}
+            />
+            <ButtonConvert
+              onPress={() => {
+                setCurrentUnit(oppositeUnit);
+              }}
+              unit={currentUnit}
+            />
+          </View>
+        </SafeAreaView>
+      </ImageBackground>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
